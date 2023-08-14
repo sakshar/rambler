12805e752cf496c63a7a974371cec1a3a07c694b
sakshar <sakshar@cse.buet.ac.bd>
2023-08-14 09:09:45 -0700

    corrected script "extractUnikmers.sh"

--- scripts/extractUnikmers.sh
+++ scripts/extractUnikmers.sh
@@ -2,23 +2,17 @@
 # ------- PRE-REQUISITES
 # Install the following packages in your conda environment before running this script
 # jellyfish (channel: bioconda), xlsxwriter (channel: conda-forge)
-# command to install jellyfish:
+# command to install them:
 # conda install -c bioconda jellyfish
 # conda install -c conda-forge xlsxwriter
 
 # ------- SPECIFICATION
-# READ_FILE="path_to_HiFi_reads_fasta_file"
-# k="kmer_length"
-# OUT_COUNT="path_to_output_from_count_command"
-# OUT_HISTO="path_to_output_from_histo_command"
-# OUT_XCEL="path_to_output_from_freq_distribution_kmers.py"
-# UNIKMERS="path_to_output_from_dump_command"
-READ_FILE="../data/10000_5_100.fasta"
-k=21
-OUT_COUNT="../data/test.jellyfish"
-OUT_HISTO="../data/test.histo"
-OUT_XCEL="../data/test.xlsx"
-UNIKMERS="../data/test.kmers"
+READ_FILE="path_to_HiFi_reads_fasta_file"
+k="kmer_length"
+OUT_COUNT="path_to_output_from_count_command"
+OUT_HISTO="path_to_output_from_histo_command"
+OUT_XCEL="path_to_output_from_freq_distribution_kmers.py"
+UNIKMERS="path_to_output_from_dump_command"
 
 # the count and histo commands together generate the frequency distribution of kmers
 # in the $OUT_HISTO file 
@@ -37,6 +31,3 @@ python3 ./freq_distribution_kmers.py $OUT_HISTO $OUT_XCEL
 # lower="put round(mean - 3*sd) here as expects an integer value"
 # upper="put round(mean + 3*sd) here as expects an integer value"
 # jellyfish dump -c -t -L $lower -U $upper $OUT_COUNT | awk '{print $1}' > $UNIKMERS
-lower=1
-upper=1
-jellyfish dump -c -t -L $lower -U $upper $OUT_COUNT | awk '{print $1}' > $UNIKMERS
